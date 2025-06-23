5c563724723fc5d5a9055fd8c53eb4d0414076c1
MadJoJoker <voegtchen@yahoo.de>
2025-06-23 12:24:02 +0200

    icons path sidebar

---
+++ js/ui/render-card.js
@@ -0,0 +1,26 @@
+  <div class="task-card">
+        <div class="task-type-tag">
+            Typ der Aufgabe
+        </div>
+
+        <h3 class="task-title">Titel der Aufgabe hier</h3>
+
+        <p class="task-description">Kurze Beschreibung der Aufgabe...</p>
+
+        <div class="subtask-progress">
+            <div class="progress-bar-container">
+                <div class="progress-bar"></div>
+            </div>
+            <p class="subtask-count">X/Y Unteraufgaben</p>
+        </div>
+
+        <div class="task-footer">
+            <div class="assigned-users">
+                <div class="user-avatar">AB</div>
+                <div class="user-avatar">CD</div>
+            </div>
+
+            <div class="priority-icon-placeholder">
+            </div>
+        </div>
+    </div>
