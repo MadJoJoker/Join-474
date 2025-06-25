1f7cae541b64c7281b14fa80835d1cb8595d5a5b
b-rich-dev <eugen.birich@gmail.com>
2025-06-25 11:22:10 +0200

    feat. Add Resize Handle and set Date

---
+++ js/events/card-events.js
@@ -0,0 +1,58 @@
+const picker = flatpickr("#datepicker", {
+    dateFormat: "d.m.Y",
+    allowInput: true
+});
+
+let isResizing = false;
+
+let startY, startHeight, currentTextarea;
+
+function formatDate(input) {
+    let value = input.value.replace(/\D/g, "");
+
+    if (value.length > 8) value = value.slice(0, 8);
+
+    let formatted = "";
+    if (value.length > 4) {
+        formatted = value.slice(0, 2) + "." + value.slice(2, 4) + "." + value.slice(4);
+    } else if (value.length > 2) {
+        formatted = value.slice(0, 2) + "." + value.slice(2);
+    } else {
+        formatted = value;
+    }
+
+    input.value = formatted;
+}
+
+
+function openPicker() {
+    picker.open();
+}
+
+
+function startResize(e) {
+    isResizing = true;
+    currentTextarea = e.target.closest('.textarea-wrapper').querySelector('textarea');
+    startY = e.clientY;
+    startHeight = currentTextarea.offsetHeight;
+
+    document.onmousemove = resizeTextarea;
+    document.onmouseup = stopResize;
+
+    e.preventDefault();
+}
+
+
+function resizeTextarea(e) {
+    if (!isResizing) return;
+
+    const newHeight = (startHeight + e.clientY - startY) + 'px';
+    currentTextarea.style.height = newHeight;
+}
+
+
+function stopResize() {
+    isResizing = false;
+    document.onmousemove = null;
+    document.onmouseup = null;
+}
