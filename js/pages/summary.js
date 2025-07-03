a89a8c13f3a4a5f0936f8c0d6c514d66634ea0d7
fidus-Achates <110263578+fidus-Achates@users.noreply.github.com>
2025-07-03 17:35:00 +0200

    create sign up page, modifications in index

--- js/pages/summary.js
+++ js/pages/summary.js
@@ -12,7 +12,28 @@ function includeHeaderAndSidebar() {
   addLayoutElements('../js/templates/sidebar.html', 'sidebar');
 }
 
-function setSmryIcon(triggeredElement, filename) {
-  const img = triggeredElement.querySelector("img");
-  img.src = '../assets/icons/sidebar/' + filename;
+// function setSmryIcon(triggeredElement, filename) {
+//   const img = triggeredElement.querySelector("img");
+//   img.src = '../assets/icons/sidebar/' + filename;
+// }
+
+async function getFirebaseData() {
+  const URL_FIREBASE_JOIN = 'https://join-474-default-rtdb.europe-west1.firebasedatabase.app/.json';
+  try {
+    const RESPONSE_FIREBASE = await fetch(URL_FIREBASE_JOIN);
+    if (!RESPONSE_FIREBASE.ok) {
+      console.error('Network response was not ok:', RESPONSE_FIREBASE.statusText);
+      return null;
+    }
+    const DATA_FIREBASE_JOIN = await RESPONSE_FIREBASE.json();
+
+    console.log(DATA_FIREBASE_JOIN);
+
+    return DATA_FIREBASE_JOIN;
+  } catch (error) {
+    console.error('There was a problem with your fetch operation:', error);
+    return null;
+  }
 }
+
+getFirebaseData();
